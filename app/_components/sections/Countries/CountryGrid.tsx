2087782b667f779c97e388d1eee2b93dd1640fce
okaaltero <oka@alterolab.com>
2024-05-29 10:24:05 +0800

    fix: removes fetching from local file for country data

--- app/_components/sections/Countries/CountryGrid.tsx
+++ app/_components/sections/Countries/CountryGrid.tsx
@@ -16,6 +16,14 @@ interface CountryGridProps {
 export const CountryGrid = ({ countries }: CountryGridProps) => {
   const { filteredCountries } = useCountryGrid({ countries });
 
+  if (filteredCountries.length === 0) {
+    return (
+      <div className="flex items-center justify-center rounded-lg bg-zinc-900 px-5 py-10 text-white">
+        No countries available
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="grid grid-cols-1 gap-x-3 gap-y-4 md:grid-cols-3 lg:grid-cols-4">
