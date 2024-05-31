7f469b1034fb4a4963dcb726ec3778127d500704
Oka Wibawa <okaa.wibawa@gmail.com>
2024-05-31 15:46:52 +0800

    refactor: reverts back destination country grid structure

--- app/_components/sections/Home/DestinationCountriesSection/CountryGrid.tsx
+++ app/_components/sections/Home/DestinationCountriesSection/CountryGrid.tsx
@@ -5,9 +5,5 @@ import { CountryCard } from "./CountryCards";
 export const CountryGrid = async () => {
   const countries = await readFile("./countries.json", "utf8");
 
-  return (
-    <div className="grid grid-cols-1 gap-x-3 gap-y-4 md:grid-cols-3 lg:grid-cols-4">
-      <CountryCard countries={JSON.parse(countries)} />
-    </div>
-  );
+  return <CountryCard countries={JSON.parse(countries)} />;
 };
