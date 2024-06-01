597eac7671fb52a89435f6a34dc54702640b9fdd
Oka Wibawa <okaa.wibawa@gmail.com>
2024-06-01 15:36:12 +0800

    feat: wip plan details page

---
+++ app/(pages)/esims/[slug]/plan-details/page.tsx
@@ -0,0 +1,11 @@
+export default function EsimsDetailPlan({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  return (
+    <main className="full-width bg-black">
+      <h1>Your Purchase</h1>
+    </main>
+  );
+}
